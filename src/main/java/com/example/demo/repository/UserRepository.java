6eaac089d78bfe896c6f204df08d788bc9328a37
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2019-09-16 12:48:39 +0200

    Security Implementation

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -9,7 +9,7 @@ import com.example.demo.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User> findByUserName(String username);
+	Optional<User> findByUsername(String username);
 	Boolean existsByUsername(String username);
 	Boolean existsByEmail(String email);	
 }
---
+++ src/main/java/com/example/demo/security/services/UserPrincipal.java
@@ -0,0 +1,131 @@
+package com.example.demo.security.services;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.demo.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class UserPrincipal implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	private String name;
+	
+	private String username;
+	
+	private String email;
+	
+	@JsonIgnore
+	private String password;
+	
+	private Collection<? extends GrantedAuthority> authorities;
+
+	public UserPrincipal(Long id, String name, String username, String email, String password,
+			Collection<? extends GrantedAuthority> authorities) {
+		this.id = id;
+		this.name = name;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.authorities = authorities;
+	}
+	
+	public static UserPrincipal build(User user) {
+		List<GrantedAuthority> authorities = user
+			.getRoles()
+			.stream()
+			.map(role -> new SimpleGrantedAuthority(role.getName().name())
+		).collect(Collectors.toList());
+		
+		return new UserPrincipal(
+				user.getId(),
+				user.getName(),
+				user.getUsername(), 
+				user.getEmail(), 
+				user.getPassword(), 
+				authorities
+		);
+	}
+	
+	public Long getId() {
+        return id;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public String getEmail() {
+        return email;
+    }
+ 
+    @Override
+    public String getUsername() {
+        return username;
+    }
+ 
+    @Override
+    public String getPassword() {
+        return password;
+    }
+ 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+ 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+ 
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+ 
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+ 
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    
+    
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserPrincipal other = (UserPrincipal) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
